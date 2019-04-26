52e5b1b39b09f17c61a39b894c0b0c63bbb2ddfa
Russell <russell.t.sherman@gmail.com>
2019-04-26 18:34:20 -0500

    packer copy users ssh public key

--- scripts/cloud-init.sh
+++ scripts/cloud-init.sh
@@ -123,11 +123,8 @@ YAML
 # Create user-data
 sudo tee /boot/user-data <<'YAML'
 #cloud-config
-ssh_authorized_keys:
- - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4QCqtjvfm3Ih+pIQ0cS1NmHIpzhO2/p4xBSk/dzyJxXaljygwxdE+19qLh11HKK2tlZXBjGAb6JIPm+QbiIFdSzc/74+Ls+TiUIWKicygqniWn1cJHsREZ1WuMjzyzA4yfdkjDkmQlFZlkaWMOE8yYaZl3R63h+mD4klMJoAFdeONqABwXNW7Kt3QcMnYH+k+wR09MyhWwxZgpzrjPaenfGW010YSG3MsbFkSCQMjZOBFSbIkNRc0yXL+ENZvI+JVYA1HVNES5roFqRI9j1McrAZgkHuvz83IcDe5oFbvZq2s7nfwNYTXw0vsKjGDSAkLbc/Uh40QuUJ4rkejuuqb russell.t.sherman@gmail.com
 
 users:
-  - default
   - name: devops
     sudo: ALL=(ALL) NOPASSWD:ALL
     groups: adm, dialout, cdrom, sudo, audio, video, plugdev, games, users, input, netdev, gpio, i2c, spi
