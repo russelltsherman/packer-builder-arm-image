1234744e99962ae056ee7b97c0d275d5a8d7d3d6
Russell <russell.t.sherman@gmail.com>
2019-04-28 18:51:35 -0500

    add enable uart

---
+++ scripts/enable_uart.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+CONFIG=/boot/config.txt
+
+# Enable OTG in config
+if [ -e $CONFIG ] && grep -q "enable_uart=1" $CONFIG; then
+  echo "enable_uart=1 present in $CONFIG"
+
+  if [ -e $CONFIG ] && grep -q "^#enable_uart=1$" $CONFIG; then
+    echo "uncommenting"
+    sed -i -e "s|#enable_uart=1|enable_uart=1|" $CONFIG
+  fi
+else
+  echo "enable_uart=1 added to $CONFIG"
+  printf "\n#enable serial console\nenable_uart=1" | tee -a $CONFIG
+fi
