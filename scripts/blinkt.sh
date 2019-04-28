f7192f19403e0506be69240b2c1491e3aa934515
Russell <russell.t.sherman@gmail.com>
2019-04-27 22:09:53 -0500

    enable autorunning blinkt revolving example

--- scripts/blinkt.sh
+++ scripts/blinkt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export GOPATH=/home/pi/
+export GOPATH=/home/pi
 
 github_clone() {
   provider="github.com"
@@ -13,9 +13,27 @@ github_clone() {
 github_clone russelltsherman gpio
 github_clone russelltsherman blinkt
 github_clone russelltsherman blinkt_examples
+github_clone cckuailong colorsys-go
 
 chown -R pi:pi "$GOPATH/src/"
 
 # fix permissions issue with userspace control of gpio
 chgrp -R gpio /sys/class/gpio
 chmod -R g+rw /sys/class/gpio
+
+echo "writing /etc/systemd/system/revolving.service"
+cat > /etc/systemd/system/revolving.service << 'EOL'
+[Unit]
+Description=Blinkt Color revolving
+
+[Service]
+Environment=GOPATH=/home/pi
+Environment=GOCACHE=/home/pi/.cache/go-build
+ExecStart=/usr/local/bin/go run /home/pi/src/github.com/russelltsherman/blinkt_examples/revolving/app.go
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+echo "enable revolving.service"
+systemctl enable revolving
