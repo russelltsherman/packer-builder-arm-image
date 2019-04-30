09232d01f7deb94433bd283f1d7d66f236d7b13d
Russell <russell.t.sherman@gmail.com>
2019-04-30 08:28:02 -0500

    allow more var log space

--- scripts/disable_swap.sh
+++ scripts/disable_swap.sh
@@ -7,5 +7,5 @@ systemctl disable dphys-swapfile
 sudo update-rc.d dphys-swapfile remove
 
 # /var/log and /tmp to in memory file system to prevent excessive sd write
-echo "tmpfs    /var/log    tmpfs   size=10M,noatime     0   0" | tee -a /etc/fstab
+echo "tmpfs    /var/log    tmpfs   size=100M,noatime    0   0" | tee -a /etc/fstab
 echo "tmpfs    /tmp        tmpfs   size=100M,noatime    0   0" | tee -a /etc/fstab
